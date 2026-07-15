e8ca662b8b7db437ca7f05e7a3513008838c91b6
haex <hexxx@ok.de>
2026-07-15 10:47:10 +0200

    chore: release main

--- src/fwbg/__init__.py
+++ src/fwbg/__init__.py
@@ -4,7 +4,7 @@ FWBG - Forex/Trading Strategy Optimizer.
 Plugin-basierte Architektur für Trading-Strategien.
 """
 
-__version__ = "2.14.0"
+__version__ = "2.15.0"
 
 from .core.registry import discover_plugins
 
